7c16e91298b98b2ac71d63c0a8e26993fcc91a86
Jordi Sensada Torrres <40217285+jsensada@users.noreply.github.com>
2023-11-24 11:23:55 +0100

    Adding the app-production script

---
+++ app-production.py
@@ -0,0 +1,19 @@
+from flask import Flask, request
+import os
+import logging
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_instance():
+    app.logger.info('Processing request for instance %s', app.instance_name)
+    return "Hello from " + app.instance_name
+
+if __name__ == '__main__':
+    app.instance_name = os.getenv('INSTANCE') if os.getenv('INSTANCE') else "No instance provided"
+    port = os.getenv('PORT') if os.getenv('PORT') else 5000
+    logfile = os.getenv('LOGFILE') if os.getenv('LOGFILE') else 'app.log'
+
+    logging.basicConfig(filename=logfile, level=logging.INFO)
+
+    app.run(debug=True, port=port, host='0.0.0.0')
