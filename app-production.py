5769c9584db4d34ea0904be36c0bfc1e3004087f
Jordi Sensada Torrres <40217285+jsensada@users.noreply.github.com>
2023-11-24 11:46:30 +0100

    FIX | app-production need vars outside

--- app-production.py
+++ app-production.py
@@ -9,11 +9,11 @@ def hello_instance():
     app.logger.info('Processing request for instance %s', app.instance_name)
     return "Hello from " + app.instance_name
 
+app.instance_name = os.getenv('INSTANCE') if os.getenv('INSTANCE') else "No instance provided"
+port = os.getenv('PORT') if os.getenv('PORT') else 5000
+logfile = os.getenv('LOGFILE') if os.getenv('LOGFILE') else 'app.log'
+
+logging.basicConfig(filename=logfile, level=logging.INFO)
+
 if __name__ == '__main__':
-    app.instance_name = os.getenv('INSTANCE') if os.getenv('INSTANCE') else "No instance provided"
-    port = os.getenv('PORT') if os.getenv('PORT') else 5000
-    logfile = os.getenv('LOGFILE') if os.getenv('LOGFILE') else 'app.log'
-
-    logging.basicConfig(filename=logfile, level=logging.INFO)
-
     app.run(debug=True, port=port, host='0.0.0.0')
